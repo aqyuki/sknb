3cc14c15824de048c8e7bb2e08847479e731a59f
aqyuki <yksk171118@gmail.com>
2025-09-23 22:33:13 +0900

    test(task): モックを追加

--- pkg/task/model/task/repository.go
+++ pkg/task/model/task/repository.go
@@ -2,6 +2,7 @@ package task
 
 import "context"
 
+//go:generate go tool mockgen -source ./repository.go -destination ./repository_mock.go -package task -typed
 type Repository interface {
 	CreateAndReturn(ctx context.Context, draft *DraftTask) (*Task, error)
 }
