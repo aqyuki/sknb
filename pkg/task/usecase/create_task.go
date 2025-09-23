07bd5c6f3fc6a38e7a03b1d06cec84716f7543e3
aqyuki <yksk171118@gmail.com>
2025-09-23 22:33:31 +0900

    feat(task): タスクを作成するusecaseを作成

---
+++ pkg/task/usecase/create_task.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/aqyuki/sknb/pkg/task/model/task"
+	"github.com/rs/xid"
+)
+
+type CreateTaskInput struct {
+	UserID      string
+	Title       string
+	Description string
+}
+
+type CreateTask struct {
+	repo task.Repository
+}
+
+func NewCreateTask(repo task.Repository) *CreateTask {
+	return &CreateTask{repo: repo}
+}
+
+func (u *CreateTask) Invoke(ctx context.Context, input *CreateTaskInput) (*task.Task, error) {
+	header := &task.Header{
+		ID:     xid.New().String(),
+		UserID: input.UserID,
+	}
+	body := &task.Body{
+		Title:       input.Title,
+		Description: input.Description,
+		Status:      task.StatusToDo,
+	}
+	draft := task.NewDraftTask(header, body)
+
+	return u.repo.CreateAndReturn(ctx, draft)
+}
